9285f7dc8de75a3237859d20e0e5c86475168277
Nagaraj <nagarajuande255@gmail.com>
2025-06-16 23:58:30 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -8,6 +8,11 @@ variable "instance_type" {
   type        = string
   default     = "t3.micro"
   description = "Instance type for the EC2 Instance, can be overridden per environment"
+
+  validation { # Validate the instance type
+    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type) # List of valid instance types
+    error_message = "Instance type must be a valid AWS instance type are : t3.micro, t3.small, t3.medium" # Error message if validation fails
+  }
 }
 
 #mandatory variables to be passed from the command line
